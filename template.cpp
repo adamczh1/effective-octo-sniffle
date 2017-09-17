e26d3c1b023f8c1d0af5de1f6e17cc0ec86b8154
adamczh1 <adamczheng2013@gmail.com>
2017-09-16 22:06:58 -0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <cstdio> 
+#include <iostream> 
+#include <cmath> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <functional> 
+#include <utility> 
+#include <set> 
+#include <map> 
+#include <queue> 
+#include <cstdlib> 
+#include <ctime> 
+#include <cstring> 
+#include <cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+#define A first
+#define B second
+#define SIZE(x) ((int)(x.size()))
+template<class T> T sqr(const T &x) { return x*x; }
+template<class T> int popcount(const T &n) { return (n==0)?0:(1+popcount(n&(n-1))); }
+template<class T> void ckmin(T &a,const T &b) { if (b<a) a=b; }
+template<class T> void ckmax(T &a,const T &b) { if (b>a) a=b; }
+int main(){
+	return 0;
+}
