c8bb37474d438c2fe7a75f5f43e1938d5f12ebd4
adamczh1 <adamczheng2013@gmail.com>
2017-09-16 18:34:43 -0700

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,13 +1,13 @@
 struct dsu{
     int par[MAXN];
     void init(){
-        for(int i=0;i<MAXN;i++)par[i]=i;
+        for(int i=0;i<MAXN;i++)par[i]=-1;
     }
     int find(int v){
         return par[v]<0?v:(par[v]=find(par[v]));
     }
-    void merge(int a,b){
-        if((a=find(a))==(b=find(b)))return;
+    void merge(int x,int y){
+        if((x=find(x))==(y=find(y)))return;
         if(par[y]<par[x])swap(x,y);
         par[x]+=par[y];
         par[y]=x;
