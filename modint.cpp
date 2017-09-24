3f90f5ace3d3cd6d5ee807b512ff6a9edd8e554e
adamczh1 <adamczheng2013@gmail.com>
2017-09-24 11:57:59 -0700

    Update modint.cpp

--- modint.cpp
+++ modint.cpp
@@ -1,18 +1,45 @@
-template<int MOD>
 struct ModInt {
-	static const int Mod = MOD;
+	static int Mod;
 	unsigned x;
 	ModInt() : x(0) { }
-	ModInt(signed sig) { int sigt = sig % MOD; if (sigt < 0) sigt += MOD; x = sigt; }
-	ModInt(signed long long sig) { int sigt = sig % MOD; if (sigt < 0) sigt += MOD; x = sigt; }
+	ModInt(signed sig) { int sigt = sig % Mod; if (sigt < 0) sigt += Mod; x = sigt; }
+	ModInt(signed long long sig) { int sigt = sig % Mod; if (sigt < 0) sigt += Mod; x = sigt; }
 	int get() const { return (int)x; }
 
-	ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
-	ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
-	ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+	ModInt &operator+=(ModInt that) { if ((x += that.x) >= (unsigned)Mod) x -= Mod; return *this; }
+	ModInt &operator-=(ModInt that) { if ((x += Mod - that.x) >= (unsigned)Mod) x -= Mod; return *this; }
+	ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % Mod; return *this; }
+	ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
 
 	ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
 	ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
 	ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+	ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+
+	ModInt inverse() const {
+		signed a = x, b = Mod, u = 1, v = 0;
+		while (b) {
+			signed t = a / b;
+			a -= t * b; std::swap(a, b);
+			u -= t * v; std::swap(u, v);
+		}
+		if (u < 0) u += Mod;
+		ModInt res; res.x = (unsigned)u;
+		return res;
+	}
+
+	bool operator==(ModInt that) const { return x == that.x; }
+	bool operator!=(ModInt that) const { return x != that.x; }
+	ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
 };
-typedef ModInt<1000000007> mint;
+int ModInt::Mod = 1000000007;
+typedef ModInt mint;
+mint operator^(mint a, unsigned long long k) {
+	mint r = 1;
+	while (k) {
+		if (k & 1) r *= a;
+		a *= a;
+		k >>= 1;
+	}
+	return r;
+}
