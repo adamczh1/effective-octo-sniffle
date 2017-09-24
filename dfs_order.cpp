1bb0fbc8de55244ee1d968c84068664849742efe
adamczh1 <adamczheng2013@gmail.com>
2017-09-24 13:35:47 -0700

    Update dfs_order.cpp

--- dfs_order.cpp
+++ dfs_order.cpp
@@ -19,7 +19,30 @@ void tree_getorder(const vector<vector<int> > &g, int root) {
 		}
 	}
 }
+vector<int> t_parent;
+vector<int> t_ord;
 
+template<typename T>
+void wtree_getorder(const vector<vector<pair<int, T> > > &gw, int root, vector<T> &t_weight) {
+	int n = (int)gw.size();
+	t_parent.assign(n, -1);
+	t_ord.clear();
+	t_weight.assign(n, T());
+
+	vector<int> stk; stk.push_back(root);
+	while (!stk.empty()) {
+		int i = stk.back(); stk.pop_back();
+		t_ord.push_back(i);
+		for (int j = (int)gw[i].size() - 1; j >= 0; j --) {
+			int c = gw[i][j].first;
+			if (t_parent[c] == -1 && c != root) {
+				t_parent[c] = i;
+				t_weight[c] = gw[i][j].second;
+				stk.push_back(c);
+			}
+		}
+	}
+}
 
 // euler tour order
 int t_TIME = 0;
