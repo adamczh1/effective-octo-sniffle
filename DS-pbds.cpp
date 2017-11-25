f26627fe129bb9a9a27d17088e3cfa36ee8f87bf
adamczh1 <adamczheng2013@gmail.com>
2017-11-25 10:23:15 -0800

    Update and rename pbds.cpp to DS-pbds.cpp

--- DS-pbds.cpp
+++ DS-pbds.cpp
@@ -4,3 +4,6 @@
 using namespace std;
 using namespace __gnu_pbds;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>ordered_set;
+
+find_by_order() // iterator to k-th smallest element (0 indexed) 
+X.order_of_key() // # of items strictly smaller than value
