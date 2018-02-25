2f2c162862fd926d64857c43dcc85ea1023a9394
adamczh1 <adamczheng2013@gmail.com>
2018-02-24 22:04:10 -0800

    Update DS-dsu.cpp

--- DS-dsu.cpp
+++ DS-dsu.cpp
@@ -1,6 +1,6 @@
 struct UnionFind {
 	vector<int> data;
-	void init(int n) { data.assign(n, -1); }
+	void init(int n) { data.assign(n+1, -1); }
 	bool unionSet(int x, int y) {
 		x = root(x); y = root(y);
 		if (x != y) {
@@ -12,4 +12,17 @@ struct UnionFind {
 	bool findSet(int x, int y) { return root(x) == root(y); }
 	int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
 	int size(int x) { return -data[root(x)]; }
+};
+template<int SZ> struct DSU {
+	int fa[SZ];
+	void init(){ memset(fa,-1,sizeof fa); }
+	int find(int x){ return fa[x]<0 ? x : fa[x]=find(fa[x]); }
+	int size(int x){ return -fa[find(x)]; }
+	void unite(int x,int y){
+		x=find(x), y=find(y);
+		if(x==y) return;
+		if(size(y)>size(x)) swap(x,y);
+		fa[x]+=fa[y];
+		fa[y]=x;
+	}
 };
