86f780e0b9c01e6eb99a424711a6aad4c78b247c
adamczh1 <adamczheng2013@gmail.com>
2018-04-11 16:39:41 -0700

    Create GRAPH-scc.cpp

---
+++ GRAPH-scc.cpp
@@ -0,0 +1,24 @@
+void strongconnect(const vector<vi> &g,int cur,int &t){
+	num[cur]=low[cur]=++t;
+	stk.push_back(cur);
+	onstk[cur]=1;
+	for(int v:g[cur]){
+		if(!num[v]){
+			strongconnect(g,v,t);
+			low[cur]=min(low[cur],low[v]);
+		}
+		else if(onstk[v]){ // could also be else doesn't matter
+			low[cur]=min(low[cur],num[v]);
+		}
+	}
+	if(low[cur]==num[cur]){
+		int w;
+		do{
+			w=stk.back();
+			stk.pop_back();
+			onstk[w]=0;
+			scc[w]=scccnt;
+		}while(w!=cur);
+		scccnt++;
+	}
+}
