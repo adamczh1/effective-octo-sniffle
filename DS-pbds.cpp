a66968a1adde2a96acef10a6ec6bf656775e7685
adamczh1 <adamczheng2013@gmail.com>
2018-06-29 23:58:17 -0700

    Delete DS-pbds.cpp

--- DS-pbds.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>ordered_set;
-
-find_by_order() // iterator to k-th smallest element (0 indexed) 
-X.order_of_key() // # of items strictly smaller than value
